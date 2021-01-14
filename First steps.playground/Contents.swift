99527a0f751c5afa1ce9de9f97e9c93dbcf528f8
Evan Armstrong <earmstrong@lcs.on.ca>
2021-01-14 10:29:58 -0500

    First steps of first steps.  Day #6 Thursday the 14th.

---
+++ First steps.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Cocoa
+
+var str = "Hello, playground"
+str = "goodbye"
+
+// var creates a new variable
+
+// in this str is a variable and we can change what it equals
+
+// Variables are places where you can store program data
+// Called variables because they cna vary
+// you can change the variables easily
+
+// Strings and Integers
+// On a whole number swift will asign the type Int which is short for integer
+
