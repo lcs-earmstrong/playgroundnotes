9a33832e857d0f1834bbbe610bf81e12d9271802
Evan Armstrong <earmstrong@lcs.on.ca>
2021-01-18 11:54:56 -0500

    A look at strings updated (written in data types playground)

--- Data Types.playground/Contents.swift
+++ Data Types.playground/Contents.swift
@@ -15,3 +15,27 @@ UInt8.min
 //
 //... the first digit counts for 2^0 or 1.
 //... in a 8 bit value, the final digit counts for 2^7
+
+let exercise1 = "Evan Armstrong"
+
+for singleCharaacter in exercise1{
+    print(singleCharaacter)
+}
+
+
+// Exercise 2 begins
+
+print (" exercise 2 begins ")
+
+let aQuestion = "请问现在几点了?"
+
+// Method one =  from start of string
+
+let indexRelativeToStart = aQuestion.index(aQuestion.startIndex, offsetBy:3)
+print(aQuestion[indexRelativeToStart])
+
+// Method 2 = from end of string
+
+let indexRelativeToEnd = aQuestion.index(aQuestion.endIndex, offsetBy: -5)
+
+print (aQuestion[indexRelativeToEnd])
